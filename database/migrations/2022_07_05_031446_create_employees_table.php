b5adc66ea3c3a1949a35a9bde2bc25d1cca699a9
santosh kumar pandit <skpandit659@gmail.com>
2022-07-05 09:23:57 +0545

    first

---
+++ database/migrations/2022_07_05_031446_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('department_id')->constrained('departments')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('name');
+            $table->string('employee_number');
+            $table->string('email');
+            $table->string('contact');
+            $table->string('designation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
